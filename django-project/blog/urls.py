e7f10ba755daa3b760a7da81fe733c1fe3370b69
RedPciosDamian <redeterminacionesdepreciosrdi@gmail.com>
2025-08-04 18:39:35 -0300

    Actualizar la ruta de inclusión de URLs para la aplicación Post y mejorar la configuración de archivos estáticos en modo DEBUG

--- django-project/blog/urls.py
+++ django-project/blog/urls.py
@@ -16,8 +16,13 @@ Incluyendo otra configuración de URL
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.post.urls')),
+    path('post/', include('apps.post.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
